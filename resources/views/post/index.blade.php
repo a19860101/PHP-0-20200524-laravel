2b2baf957951b73ca31046d940fbe50b17811b8c
Zac <a19860101@gmail.com>
2020-07-19 16:00:59 +0800

    20200719

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,21 +4,37 @@
     我是首頁
 @endsection
 @section('main')
-    <h1>INDEX</h1>
-    @foreach($posts as $post)
-        <h2>{{$post->title}}</h2>
-        <div>
-            {{$post->content}}
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>文章首頁</h1>
+                <table class="table">
+                    <tr>
+                        <th>#</th>
+                        <th>文章標題</th>
+                        <th>建立時間</th>
+                        <th>最後更新時間</th>
+                        <th>動作</th>
+                    </tr>
+                    @foreach($posts as $post)
+                    <tr>
+                        <td>{{$post->id}}</td>
+                        <td>{{$post->title}}</td>
+                        <td>{{$post->created_at}}</td>
+                        <td>{{$post->updated_at}}</td>
+                        <td>
+                            <form action="{{route('post.destroy',['id' => $post->id])}}" method="post" class="d-inline">
+                                @csrf
+                                @method('delete')
+                                <input type="submit" value="刪除" class="btn btn-danger btn-sm">
+                            </form>
+                            <a href="{{route('post.show',['id' => $post->id])}}" class="btn btn-success btn-sm">檢視</a>
+                            <a href="{{route('post.edit',['id' => $post->id])}}" class="btn btn-primary btn-sm">編輯</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
         </div>
-        <div>
-            最後更新時間:{{$post->updated_at}}
-        </div>
-        <form action="{{route('post.destroy',['id' => $post->id])}}" method="post">
-            @csrf
-            @method('delete')
-            <input type="submit" value="刪除">
-        </form>
-        <a href="{{route('post.show',['id' => $post->id])}}">檢視</a>
-        <a href="{{route('post.edit',['id' => $post->id])}}">編輯</a>
-    @endforeach
+    </div>
 @endsection
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -4,13 +4,18 @@
 文章標題{{$post->title}}
 @endsection
 @section('main')
-
-<h1>SHOW</h1>
-    <h2>{{$post->title}}</h2>
-    <div>
-        {{$post->content}}
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>{{$post->title}}</h1>
+            <div>
+                {{$post->content}}
+            </div>
+            <div>
+                最後更新時間:{{$post->updated_at}}
+            </div>
+            <a href="#" class="btn btn-primary btn-sm" onclick="history.back()">回上頁</a>
+        </div>
     </div>
-    <div>
-        最後更新時間:{{$post->updated_at}}
-    </div>
-    @endsection
+</div>
+@endsection
