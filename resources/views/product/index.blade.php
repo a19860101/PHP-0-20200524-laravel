b9d0de5412662e89474d8fa2553aa4bdca061297
Zac <a19860101@gmail.com>
2020-07-26 12:20:42 +0800

    20200726

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -3,4 +3,14 @@
 產品列表
 @endsection
 @section('main')
+<div class="container">
+    <div class="row">
+        @foreach($products as $prod)
+        <div class="col-3">
+            <img src="/storage/images/{{$prod->img}}" class="w-100">
+            <h2>{{$prod->title}}
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
